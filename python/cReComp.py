2556c3fe353fc51afb2d4831308267547eae0d54
Kazushi Yamashina <kazushi@virgo.is.utsunomiya-u.ac.jp>
2016-01-19 17:14:26 +0900

    added scrp_conf.py

--- python/cReComp.py
+++ python/cReComp.py
@@ -1,14 +1,44 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
+# NAME
+#         "cReComp.py"
+# DESCRIPTION
+#         This is a code generator for Reconfigurable Component
+# 		  creator Reconfigurable HW componet
+# VERSION
+#         0.10
+# LICENCE
+#		  new BSD
+#
+# (c) Kazushi Yamashina
 import sys
 sys.path.append("lib/")
 import check_option
+import scrp_conf
 
 def read_lib(fo,str):
 	fi = open(str,"r")
 	for line in fi:
 		fo.write(line)
 		fi.close
+
+
+# 		self.module_name = ""
+# 		self.module_type = ""
+# 		self.use_fifo_32 = False
+# 		self.use_fifo_8 = False
+# 		self.option_port = False
+# 		self.port_stack = []
+# 		self.make_32_alw = False
+# 		self.alw32_stack = []
+# 		self.make_8_alw = False
+# 		self.alw8_stack = []
+# 		self.sub_module = False
+# 		self.sub_module_name = []
+# 		self.assign_target_module = []
+# 		self.assign_port_stack = []
+# 		self.fi=dsl_file
+# 		self.line = 1
 
 if __name__ == "__main__":
 	
@@ -26,5 +56,8 @@ if __name__ == "__main__":
 	check_option.option(argv0,argv1)
 
 	dsl_file = argv0
+	flag = scrp_conf.ConfigFlag(dsl_file)
+	flag.check_format(dsl_file)
+	flag.set(dsl_file)
 
 	
---
+++ python/lib/scrp_conf.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class ConfigFlag(object):
+	"""docstring for ConfigFlag"""
+	def __init__(self,dsl_file):
+		self.module_name = ""
+		self.module_type = ""
+		self.use_fifo_32 = False
+		self.use_fifo_8 = False
+		self.option_port = False
+		self.port_stack = []
+		self.make_32_alw = False
+		self.alw32_stack = []
+		self.make_8_alw = False
+		self.alw8_stack = []
+		self.sub_module = False
+		self.sub_module_name = []
+		self.assign_target_module = []
+		self.assign_port_stack = []
+		self.fi=dsl_file
+		self.line = 1
+
+	def elem_ins(self):
+		array = []
+		i = 0
+		l = ""
+		while True:
+		 	l = self.fi.readline().rstrip()
+			if l == "}":
+				self.line = self.line + 1
+				break
+			elif l != "{" and l != "}":
+				array.append(l.translate(None,"\t "))
+				i = i + 1
+			self.line = self.line + 1
+		return array
+
+	def check_format(self,filename):
+		str_ = filename.split(".")
+		if not str_[1] == "scrp":
+			print "Error! This file format is not supported by cReComop"
+			quit()
+
+	def set(self,dsl_file):
+		self.fi = open(dsl_file)
+		i_sub = 0
+		while True:
+			line = self.fi.readline().translate(None,"{\t")
+			state = line.rstrip().split(" ")
+			# print state
+			if "//" in state[0]:
+				pass
+			elif "module_name" == state[0]:
+				self.module_name = state[1]
+				# print self.module_name
+			elif "module_type" == state[0]:
+				self.module_type = state[1]
+				# print self.module_type
+			elif "use_fifo_32" == state[0]:
+				self.use_fifo_32 = True
+				# print self.use_fifo_32
+			elif "use_fifo_8" == state[0]:
+				self.use_fifo_8 = True
+				# print self.use_fifo_8
+			elif "option_port" == state[0]:
+				self.option_port = True
+				self.port_stack = self.elem_ins()
+				# print self.port_stack
+			elif "make_32_alw" == state[0]:
+				self.make_32_alw = True
+				self.alw32_stack = self.elem_ins()
+				# print self.alw32_stack
+			elif "make_8_alw" == state[0]:
+				self.make_8_alw = True
+				self.alw8_stack = self.elem_ins()
+				# print self.alw8_stack
+			elif "sub_module_name" == state[0]:
+				self.sub_module = True
+				string = state[1] + " " + state[2]
+				self.sub_module_name.append(string)
+				# print self.sub_module_name
+			elif "assign_port" == state[0]:
+				while True:
+					l = self.fi.readline().rstrip()
+					if l == "}":
+						self.line = self.line + 1
+						break
+					elif l != "{" and l != "}":
+						self.assign_port_stack.append(l.translate(None," \t"))
+					self.line = self.line + 1
+					# print self.assign_port_stack
+			elif "end" in state[0]:
+				break
+			else:
+				print "Syntax Error near line %s in %s"%(self.line,dsl_file)
+				quit()
+			self.line = self.line + 1
+
+if __name__ == '__main__':
+	
+	filename = "sample.scrp"
+	flag = ConfigFlag(filename)
+	flag.check_format(filename)
+	flag.set(filename)
